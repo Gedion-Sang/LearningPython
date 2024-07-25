0eb912db4b26e5fb678f52ec647971632862ecef
Gedion-Sang <sangkybet4@gmail.com>
2024-07-25 12:47:04 +0300

    New Commit

--- qrcode.py
+++ qrcode.py
@@ -1,6 +1,8 @@
 import pyqrcode
-from pyqrcode import Image
-link = input(":nEnter the URL to generate a QR code: ")
+from PIL import Image
+link = input(":/nEnter the URL to generate a QR code:")
 qr_code = pyqrcode.create(link)
 qr_code.png("QRCode.png", scale=10)
 Image.open("QRCode.png").show()
+
+# Ensure you install pyqrcode and pillow libraries before running this script

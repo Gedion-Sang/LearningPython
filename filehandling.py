334b9f4322ccbd1b275276cf85186945d38e2c23
Gedion-Sang <sangkybet4@gmail.com>
2024-07-25 13:10:21 +0300

    New Commits

---
+++ filehandling.py
@@ -0,0 +1,12 @@
+# File handling 
+fileobject = open("helloworld.py", 'w')
+
+# Write the code to the file
+
+fileobject.write("print('Hello, World!')")
+
+# Close the file
+
+fileobject.close()
+
+# Execute the code in the file
